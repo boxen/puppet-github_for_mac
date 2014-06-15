5ff1b1667fc59f073b91dfd9aeac6c2295fac771
Samuel Sieg <samuel.sieg@crafted.ch>
2014-06-15 13:03:02 +0200

    Fix symlink

--- manifests/init.pp
+++ manifests/init.pp
@@ -7,9 +7,8 @@ class github_for_mac {
     provider => 'compressed_app'
   }
 
-  file { 'github_cli_symlink':
+  file { "${boxen::config::bindir}/github":
     ensure  => link,
-    source  => "${boxen::config::bindir}/github",
     target  => '/Applications/GitHub.app/Contents/MacOS/github_cli',
     mode    => '0755',
     require => Package['GitHub']
