ca7abca4be8c7a8aea43766d0125e03bb3511dfb
Samuel Sieg <samuel.sieg@crafted.ch>
2014-06-15 12:29:44 +0200

    Create symlink for github_cli

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,4 +6,12 @@ class github_for_mac {
     source   => 'https://github-central.s3.amazonaws.com/mac/GitHub%20for%20Mac%20181.zip',
     provider => 'compressed_app'
   }
+
+  file { 'github_cli_symlink':
+    ensure  => link,
+    source  => "${boxen::config::bindir}/github",
+    target  => '/Applications/GitHub.app/Contents/MacOS/github_cli',
+    mode    => '0755',
+    require => Package['GitHub']
+  }
 }
--- spec/classes/github_for_mac_spec.rb
+++ spec/classes/github_for_mac_spec.rb
@@ -6,5 +6,9 @@ describe 'github_for_mac' do
       :source   => 'https://github-central.s3.amazonaws.com/mac/GitHub%20for%20Mac%20181.zip',
       :provider => 'compressed_app'
     })
+
+    should contain_file('github_cli_symlink').with({
+      :target => '/Applications/GitHub.app/Contents/MacOS/github_cli'
+    })
   end
 end
